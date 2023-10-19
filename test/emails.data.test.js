7651c18a90f760d3d6d1350e2fe67b0fff422fda
Lizeth Lopez <lizlopez.10@hotmail.com>
2023-10-18 22:55:18 -0500

    config yml

--- test/emails.data.test.js
+++ test/emails.data.test.js
@@ -1,12 +1,14 @@
+const express = require('express');
+const supertest = require('supertest');
+const router = require('../routes/healthcheck'); // Reemplaza con la ubicación real de tus rutas
+const app = express();
+app.use('/', router);
+const request = supertest(app);
 
-
-test('get emails', () => {
-    expect(1).toBe(1)
+describe('Rutas', () => {
+  it('Debería responder con el mensaje correcto', async () => {
+    const res = await request.get('/');
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('ok');
+  });
 });
-
-// test('post email', () => {
-//     db.query.mockImplementation("", cb => cb('valor'))
-//     data.postEmail((result)=>{
-//         expect(result).toBe('valor')
-//     })
-// });
